afd1b4686b9e2a3fb81370b7a5f26d678b4a7a92
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-24 00:02:02 +0600

    this keyword, Dot notation vs bracket notation

---
+++ dot.js
@@ -0,0 +1,13 @@
+const person = {
+    name: 'kodom ali',
+    job: 'badam khan',
+    3: 'third',
+    'add-dress': 'kochu keht'
+}
+
+const prop = 'job'
+console.log(person.job);
+console.log(person['job']);
+console.log(person[prop]);
+console.log(person['3']);
+console.log(person["add-dress"]);
